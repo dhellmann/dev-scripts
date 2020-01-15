341cc0af5238cfff5e7ab3e71b0cd2957b724bbb
Doug Hellmann <dhellmann@redhat.com>
2020-01-15 15:18:02 -0500

    update to latest nmstate
    Signed-off-by: Doug Hellmann <dhellmann@redhat.com>

--- nmstate/deploy.sh
+++ nmstate/deploy.sh
@@ -4,9 +4,11 @@ basedir=$(dirname $0)
 
 # https://github.com/kubevirt/cluster-network-addons-operator#deployment
 
-oc apply -f ${basedir}/namespace.yaml
-oc apply -f ${basedir}/network-addons-config.crd.yaml
-oc apply -f ${basedir}/operator.yaml
+oc apply -f https://raw.githubusercontent.com/kubevirt/cluster-network-addons-operator/master/manifests/cluster-network-addons/0.25.0/namespace.yaml
+
+oc apply -f https://raw.githubusercontent.com/kubevirt/cluster-network-addons-operator/master/manifests/cluster-network-addons/0.25.0/network-addons-config.crd.yaml
+
+oc apply -f https://raw.githubusercontent.com/kubevirt/cluster-network-addons-operator/master/manifests/cluster-network-addons/0.25.0/operator.yaml
 
 # https://github.com/kubevirt/cluster-network-addons-operator#nmstate
 # https://github.com/kubevirt/cluster-network-addons-operator#linux-bridge
